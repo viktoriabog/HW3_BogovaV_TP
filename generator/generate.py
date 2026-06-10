9673c5acc7f045cc891a42f993b1ee73f7d1e7e5
viktoriabog <anthonynovak2121@gmail.com>
2026-06-10 13:30:58 +0000

    испр ошибки

--- generator/generate.py
+++ generator/generate.py
@@ -10,7 +10,7 @@ def generate_row():
     return {
         "ID фильма": random.randint(1000, 9999),
         "Рейтинг": round(random.uniform(1.0, 10.0), 2),
-        "Длительность": random.randint(60, 240),
+        "Длительность в мин": random.randint(60, 240),
         "Жанр": random.choice(["Комедия", "Драма", "Боевик", "Ужасы"]),
     }
 
